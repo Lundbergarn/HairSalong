3447b964b537d53e28719a488814294591e77afa
Christoffer Lundberg <lundbergarn2@hotmail.com>
2019-10-30 14:47:34 +0100

    Updates

---
+++ precache-manifest.74123c51706a4d5482b59f5b29c80213.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8538ae657f368d312f381f2c3e9eea78",
+    "url": "/HairSalong/index.html"
+  },
+  {
+    "revision": "bd87dcac3e451d992015",
+    "url": "/HairSalong/static/css/main.cc645e24.chunk.css"
+  },
+  {
+    "revision": "59142a82c6280ad7290e",
+    "url": "/HairSalong/static/js/2.a13e19b8.chunk.js"
+  },
+  {
+    "revision": "bd87dcac3e451d992015",
+    "url": "/HairSalong/static/js/main.91e62eea.chunk.js"
+  },
+  {
+    "revision": "e3bc5806c14abf43dac8",
+    "url": "/HairSalong/static/js/runtime-main.5ea6371f.js"
+  },
+  {
+    "revision": "aff4e59d46867831fdbe8d2e462784ac",
+    "url": "/HairSalong/static/media/Font Bureau - MillerBanner Light.aff4e59d.otf"
+  },
+  {
+    "revision": "aa48c748feeb50f0482dcfd55d649566",
+    "url": "/HairSalong/static/media/image.aa48c748.png"
+  }
+]);
